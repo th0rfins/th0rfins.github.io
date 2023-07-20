089be9fe18c9c2e7bddd200c1bad755168903b17
th0rfins <140086884+th0rfins@users.noreply.github.com>
2023-07-20 22:38:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,3 @@
-<?php
-session_start();
-if(!isset($_SESSION['login'])) {
-    header('LOCATION:login/login.php'); die();
-} else {
-}
-if(isset($_POST['but_logout'])){
-
-
-
-    session_destroy();
-    header('Location: index.php');
-}
- ?>
 <!DOCTYPE html>
 <html class="loading">
 <head>
